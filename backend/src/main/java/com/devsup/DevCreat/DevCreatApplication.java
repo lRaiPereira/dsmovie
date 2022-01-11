705d412f255d15d54589cce0de302ac08f9d7741
Rai Pereira <rai.carmo@yahoo.com.br>
2022-01-10 22:03:36 -0300

    Project Creat

---
+++ backend/src/main/java/com/devsup/DevCreat/DevCreatApplication.java
@@ -0,0 +1,13 @@
+package com.devsup.DevCreat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCreatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevCreatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsup/DevCreat/DevCreatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsup.DevCreat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevCreatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
